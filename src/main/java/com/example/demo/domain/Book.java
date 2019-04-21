ad2f93634a167ddb91ccbe74f7d69ef57700ce05
hyejun8701 <hyejun8701@gmail.com>
2019-04-21 16:06:26 +0900

    test

--- src/main/java/com/example/demo/domain/Book.java
+++ src/main/java/com/example/demo/domain/Book.java
@@ -2,12 +2,15 @@ package com.example.demo.domain;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
+@Entity
 public class Book {
 	private Integer idx;
 	private String title;
