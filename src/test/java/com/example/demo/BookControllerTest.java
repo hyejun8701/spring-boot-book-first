fd9bbb1882fdab2f32bbfb9c4d46ee991a26ca4b
hyejun8701 <hyejun8701@gmail.com>
2019-04-21 16:03:40 +0900

    init

---
+++ src/test/java/com/example/demo/BookControllerTest.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import static org.hamcrest.Matchers.contains;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.controller.BookController;
+import com.example.demo.domain.Book;
+import com.example.demo.service.BookService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private BookService bookService;
+	
+	@Test
+	public void Book_MVC_Å×½ºÆ®() throws Exception {
+		Book book = new Book("Spring Boot Book", LocalDateTime.now());
+		
+		given(bookService.getBookList()).willReturn(Collections.singletonList(book));
+		
+		mvc.perform(get("/books"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("book"))
+		.andExpect(model().attributeExists("bookList"))
+		.andExpect(model().attribute("bookList", contains(book)));
+	}
+
+}
